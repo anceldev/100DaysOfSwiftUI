b90b0ca20263466b79e52cd4c001c4c05ca6be36
Ancel Dev account <anceldev@Ancels-MacBook-Pro.local>
2023-11-14 16:48:21 +0100

    CupCakeCorner Init

---
+++ CupCakeCorner/CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Ancel Dev account on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
